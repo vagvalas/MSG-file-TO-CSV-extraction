acee5b81add3ecd85d739b6482051df3dec052a1
vagvalas <vagvalas@yahoo.gr>
2022-11-29 18:58:47 +0200

    Add files via upload

---
+++ msgtocsv.py
@@ -0,0 +1,20 @@
+﻿import os
+import extract_msg
+import csv
+
+with open(r'Data.csv', mode='w') as file:
+    writer = csv.writer(file)
+
+    msgdata = []
+
+    for f in os.listdir('.'):
+        if not f.endswith('.msg'):
+            continue
+
+        msg = extract_msg.Message(f)
+        msg_sender = msg.sender
+        msg_date = msg.date
+        msg_subj = msg.subject
+        msg_message = msg.body
+        msgdata = msg_message.split()
+        writer.writerow(msgdata)
